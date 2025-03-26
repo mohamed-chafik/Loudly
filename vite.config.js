fcb01529d66468ae3184548bfd534e9521d2a27e
mohamed-chafik <mchafik2006@gmail.com>
2025-03-26 15:54:50 +0000

    for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  server: {
-    host: '0.0.0.0',
-    https: true, // Enables HTTPS
-    port: 5173,
-    strictPort: true
-  }, // <-- THIS COMMA WAS MISSING
-  build: {
-    chunkSizeWarningLimit: 1000 // Set limit to 1000 kB
-  }
+  plugins: [react()],// <-- THIS COMMA WAS MISSING
+
 })
