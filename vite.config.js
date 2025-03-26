da0fdd00f6c642e243ab25e74bfdf2dce04a2100
mohamed-chafik <mchafik2006@gmail.com>
2025-03-26 15:22:20 +0000

    fix vit config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),basicSsl()],
-server: {
+  plugins: [react(), basicSsl()],
+  server: {
     host: '0.0.0.0',
     https: true, // Enables HTTPS
     port: 5173,
     strictPort: true
-  }
-build: {
-    chunkSizeWarningLimit: 1000, // Set limit to 1000 kB
+  }, // <-- THIS COMMA WAS MISSING
+  build: {
+    chunkSizeWarningLimit: 1000 // Set limit to 1000 kB
   }
 })
